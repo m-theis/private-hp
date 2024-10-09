815b4e772d6d990d3a06d330f0845def21e785e4
Moritz Theis <moritz@snappx.io>
2024-10-09 07:38:01 +0200

    add sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,19 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
+import robotsTxt from 'astro-robots-txt';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://example.com',
+    site: 'https://moritztheis.com',
     integrations: [
         mdx(),
         sitemap(),
         tailwind({
             applyBaseStyles: false
+        }),
+        robotsTxt({
+            sitemap: false
         })
     ]
 });
